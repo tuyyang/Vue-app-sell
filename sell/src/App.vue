e57912c77ad0bdb9129217fe39adf570406a5553
tuyyang <46390022+tuyyang@users.noreply.github.com>
2019-11-17 00:01:46 +0800

    创建header框架

---
+++ sell/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <v-header></v-header>
+    </div>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      i am content
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue';
+
+  export default{
+    components: {
+      'v-header':header
+    }
+  };
+</script>
+
+<style lang="stylus">
+  #app
+    .tab
+      display: flex
+      width 100%
+      line-height: 40px
+      height: 40px
+      .tab-item
+        flex: 1
+        text-align: center
+</style>
---
+++ sell/src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+ <div class="header">
+i am header
+ </div>
+</template>
+
+<script>
+ export default{};
+</script>
+
+<style lang="stylus">
+
+ 
+</style>
