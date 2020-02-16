6db2128070c3207f41719cd67541f9d30d042087
tuyyang <46390022+tuyyang@users.noreply.github.com>
2020-02-16 20:59:32 +0800

    singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="singer" ref="singer">
-    歌手界面
+    <list-view></list-view>
   </div>
 </template>
 
 <script>
+import ListView from 'base/listview/listview'
+
 export default {}
 </script>
 
-<style lang='stylus' scoped></style>
+<style lang='stylus' scoped>
+  .singer
+    position fixed
+    top 88px
+    bottom 0
+    width 100%
+</style>
