4b24100b6882fdd520264af0b911dc70404d7e0e
tuyyang <46390022+tuyyang@users.noreply.github.com>
2020-03-10 21:35:08 +0800

    基本框架

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="player" v-show="playlist.length > 0">
-    <div class="normal-player" v-show="fullScreen">0-0</div>
-    <div class="mini-player" v-show="!fullScreen"></div>
+    <div class="normal-player" v-show="fullScreen">
+      <div class="background">
+        <img width="100%" height="100%" />
+      </div>
+      <div class="top">
+        <div class="back">
+          <i class="icon-back"></i>
+        </div>
+        <h1 class="title"></h1>
+        <h2 class="subtitle"></h2>
+      </div>
+      <div class="middle"></div>
+      <div class="bottom"></div>
+    </div>
+    <div class="mini-player" v-show="!fullScreen">
+      <div class="icon">
+        <img width="40" height="40" />
+      </div>
+      <div class="text">
+        <h2 class="name"></h2>
+        <p class="desc"></p>
+      </div>
+    </div>
   </div>
 </template>
 
