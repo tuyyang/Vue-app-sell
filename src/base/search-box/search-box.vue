1a3fb41a1dbf843014eda8feb9a4e25ca20088ad
tuyyang <46390022+tuyyang@users.noreply.github.com>
2020-03-15 17:38:22 +0800

    hotkey

--- src/base/search-box/search-box.vue
+++ src/base/search-box/search-box.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search-box">
     <i class="icon-search"></i>
-    <input class="box" :placeholder="placeholder" v-model="query" />
+    <input class="box" :placeholder="placeholder" v-model="query" ref="query" />
     <i class="icon-dismiss" v-show="query" @click="clear"></i>
   </div>
 </template>
@@ -27,6 +27,9 @@ export default {
   methods: {
     clear () {
       this.query = ''
+    },
+    setQuery (query) {
+      this.query = query
     }
   }
 }
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,15 +1,54 @@
 <template>
   <div class="search" ref="search">
     <div class="search-box-wrapper">
-      <search-box></search-box>
+      <search-box ref="searchBox"></search-box>
+    </div>
+    <div class="shortcut-wrapper">
+      <div class="shortcut">
+        <div class="hot-key">
+          <h1 class="title">热门搜索</h1>
+          <ul>
+            <li
+              class="item"
+              v-for="(item, index) in hotKey"
+              :key="index"
+              @click="addQuery(item.k)"
+            >
+              <span>{{ item.k }}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import SearchBox from 'base/search-box/search-box'
+import { getHotKey } from 'api/search'
+import { ERR_OK } from 'api/config'
 
 export default {
+  data () {
+    return {
+      hotKey: []
+    }
+  },
+  created () {
+    this._getHotKey()
+  },
+  methods: {
+    addQuery (query) {
+      this.$refs.searchBox.setQuery(query)
+    },
+    _getHotKey () {
+      getHotKey().then((res) => {
+        if (res.code === ERR_OK) {
+          this.hotKey = res.data.hotkey.slice(0, 10)
+        }
+      })
+    }
+  },
   components: {
     SearchBox
   }
@@ -23,4 +62,26 @@ export default {
   .search
     .search-box-wrapper
       margin 20px
+    .shortcut-wrapper
+      position fixed
+      top 178px
+      bottom 0
+      width 100%
+      .shortcut
+        height 100%
+        overflow hidden
+        .hot-key
+          margin 0 20px 20px 20px
+          .title
+            margin-bottom 20px
+            font-size $font-size-medium
+            color $color-text-l
+          .item
+            display inline-block
+            padding 5px 10px
+            margin 0 20px 10px 0
+            border-radius 6px
+            background $color-highlight-background
+            font-size $font-size-medium
+            color $color-text-d
 </style>
